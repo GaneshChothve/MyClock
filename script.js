0cce4d1103316928df29c946558115c4c53d2bde
Ganesh Chothve <84216414+GaneshChothve@users.noreply.github.com>
2021-08-16 17:24:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,54 @@ function rotation() {
     let s= d.getSeconds()
     let h= d.getHours()
     let m= d.getMinutes()
-    let k= parseInt(h*30) + parseInt(m/2)
+
+    let k=parseInt(h*30)+parseInt(m/4)
 
     sec.style.transform= 'rotate('+s*6+'deg)'
     min.style.transform= 'rotate('+m*6+'deg)'
     hourel.style.transform= 'rotate('+k+'deg)'
 
+}
+
+// let change color after every 5 seconds.
+
+const border1= document.querySelector('.clock')
+const border2= document.querySelector('.inner-circle')
+const dgts= document.querySelectorAll('.number')
+console.log(dgts)
+border1.style.borderColor= 'orangered'
+
+setInterval(changeColor, 1000)
+function changeColor() {
+    let k= border1.style.borderColor
+    
+    if(k ==='orangered'){ 
+        border1.style.borderColor='green'
+        border2.style.borderColor='blue'
+        dgts.forEach(setcolorto)
+        function setcolorto(i) {
+            i.style.color='red'
+        }
+        sec.style.background='orangered'
+    }
+    else if(k==='green'){
+        border1.style.borderColor = 'blue';
+        border2.style.borderColor='orangered'
+        dgts.forEach(setcolorto)
+        function setcolorto(i) {
+            i.style.color='green'
+        }
+        sec.style.background='white'
+    }
+
+
+    else{
+        border1.style.borderColor=' orangered'
+        border2.style.borderColor='green'
+        dgts.forEach(setcolorto)
+        function setcolorto(i) {
+            i.style.color='white'
+        }
+        sec.style.background='cyan'
+    }
 }
